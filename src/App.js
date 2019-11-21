d9513b7870f6122edb3f26d2adb771446d7ac611
Dennis Macharia <dennismacharia4747@gmail.com>
2019-11-21 15:36:38 +0300

    initial setup add twitter loggin module

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import TwitterLogin from 'react-twitter-auth';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  onSuccess(response) {
+    response.json().then(body => {
+      console.log(JSON.stringify(body));
+
+      alert(JSON.stringify(body));
+    });
+  }
+
+  onFailed(error) {
+    console.log(error);
+
+    alert(error);
+  }
+
+  render() {
+    return (
+      <div className="twitter">
+        <TwitterLogin
+          loginUrl="http://localhost:8080/auth/twitter"
+          onFailure={this.onFailed}
+          onSuccess={this.onSuccess}
+          requestTokenUrl="http://localhost:8080/auth/twitter/reverse"
+          forceLogin={true}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
